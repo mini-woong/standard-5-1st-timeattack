72ed359cb2eadeef6b4928c7183411fd0704b61e
mini-woong <mini_0@inu.ac.kr>
2024-05-20 15:52:14 +0900

    과제 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 export default function App() {
+  const [number, setNumber] = useState(0);
+  const [inputVal, setInputVal] = useState(0);
+  const saveInputVal = event => {
+    setInputVal(event.target.value);
+  }
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input onChange={saveInputVal}/> 만큼을
+        <button onClick={() => {setNumber(number + {inputVal});}}>더할게요</button>
+        <button onClick={() => {setNumber(number - {inputVal});}}>뺄게요</button>
+        <button onClick={() => {setNumber(0);}}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{number}</p>
       </div>
     </div>
   );
